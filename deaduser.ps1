3e1249a87e8f5cbffbef2956a4a4bf40ab6fc8db
bobbiehiv <168944227+bobbiehiv@users.noreply.github.com>
2024-08-28 18:45:16 -0400

    commit

---
+++ deaduser.ps1
@@ -0,0 +1,14 @@
+﻿
+# script for removing users in bulk
+# use a .csv file where the deader is deaduser
+
+Import-Module ActiveDirectory
+
+$Users = Import-Csv -Delimiter "," -Path "C:\powershell\deadusers.csv"
+
+foreach ($User in $Users)
+
+{
+$SAM = $User.deaduser
+Remove-ADUser $SAM -Confirm:$false
+}
