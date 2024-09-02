1414c52008392df3bf0e82c83f6ee2a1c285a164
Robert <168944227+bobbiehiv@users.noreply.github.com>
2024-09-02 10:09:19 -0400

    Update send_email.ps1

--- send_email.ps1
+++ send_email.ps1
@@ -1,27 +1,24 @@
 ﻿
 # define email varibles 
-# needed to add 2factorAuth and app password in gmail
+# needed to add 2factorAuth and app password for gmail
 
 $myEmail = "bobbiehiv@gmail.com"
 $creds = (Get-Credential -Credential $myEmail)
-$SMTP = "smtp.gmail.com"     # check $PSemailserver
+$SMTP = "smtp.gmail.com"     # check for $PSemailserver
 $To = "receiver@email.com"   # random email
-$subject = "credentials for $($User.fname) $($User.lname)"
-$port = 587
-# create a template and add credentials 
-$body = " " 
+$subject = ""                # create the subject
+$port = 587                  # SMTP port
+$body = ""                   # create the body 
 
-start-sleep 2 
+start-sleep 2                # will sleep for 2 seconds before running
 # the actual command
 Send-MailMessage -To $To -From $myEmail -Subject $subject -Body $body -SmtpServer $SMTP -Credential $creds -UseSsl -Port $port -DeliveryNotificationOption Onsuccess
 
 <#
-
 delivery notification options: 
 None - no notifications 
 Onsuccess - notifiy if successful 
 onfailure - notify is failure 
 Delay - notify if delayed 
 Never - never 
-
 #>
